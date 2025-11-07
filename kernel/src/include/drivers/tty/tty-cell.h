b5f4a57594181cbcec6b1a4303de5c96d8bce94e
QensyTeam <qensyteam@gmail.com>
2025-11-07 03:35:47 +0300

    some many fixes

--- kernel/src/include/drivers/tty/tty-cell.h
+++
@@ -1,9 +0,0 @@
-#ifndef TTY_CELL_H
-#define TTY_CELL_H
-
-#include "main-tty-struct.h"
-
-uint8_t vga_blend_color(vga_colors_t fg, vga_colors_t bg);
-uint16_t vga_cell(unsigned char uc, uint8_t color);
-
-#endif
